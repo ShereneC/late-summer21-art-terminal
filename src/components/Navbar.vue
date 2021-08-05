343b41f69efb82532c4b4bf89897e0f6defbe40d
ShereneC <spyhopper@cableone.net>
2021-08-05 17:01:52 -0600

    committing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <h1>Art-Terminal</h1>
+        <h1>A & C Blogg</h1>
       </div>
     </router-link>
     <button
